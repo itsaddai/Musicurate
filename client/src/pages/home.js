c1d9edb9ffc7f4ac78a78d33a64fbdcb4dcfd79b
itsaddai <adda9960@mylaurier.ca>
2023-07-15 22:55:19 -0400

    set up front/backend files

---
+++ client/src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Screen</h1>
+      <button>Log In</button>
+      <button>Register</button>
+    </div>
+  );
+};
+
+export default Home;
