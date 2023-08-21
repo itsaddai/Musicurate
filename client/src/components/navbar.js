9bf34565b645a5dc93fd8a91dd79c25cfdc3bd41
itsaddai <adda9960@mylaurier.ca>
2023-08-21 18:50:27 -0400

    add pages, form navbar

---
+++ client/src/components/navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
