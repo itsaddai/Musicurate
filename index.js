48723cc3f1f397cbc4381b7f9276dc032b3281d1
itsaddai <adda9960@mylaurier.ca>
2023-10-28 12:58:49 -0400

    move index.js from client/src to root

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./client/src/App";
 import { BrowserRouter } from "react-router-dom";
 //import contexts
-import { UserProvider } from "./contexts/user.context";
+import { UserProvider } from "./client/src/contexts/user.context";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
